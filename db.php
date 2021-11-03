c9304a57aa908865fedc01099841c1bdfa2cd08f
boydeveloper <wilfriedakpe3@gmail.com>
2021-11-03 04:23:59 +0100

    Initial commit

---
+++ db.php
@@ -0,0 +1,26 @@
+<?php
+
+class Dbcon{
+    public $connection;
+    private $db_host = 'localhost';
+    private $db_user = 'root';
+    private $db_password = '';
+    private $db_name = 'lawrenceonah';
+
+    public function __construct(){
+        $this->connectToDb();
+    }
+
+    public function connectToDb(){
+        $this->connection = new mysqli($this->db_host , $this->db_user , $this->db_password , $this->db_name);
+
+        if(!$this->connection){
+            echo ' Failed to connect ' . $this->connection -> connect_error;
+        } 
+    }
+
+
+}
+
+
+?>
